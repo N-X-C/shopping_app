2767cf076b2e1a95734efd5bb351c678139f09d7
tresun <tresun.m@gmail.com>
2020-04-09 09:58:37 +0545

    Shopping app fireabase resolve

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/pages/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),
+  ));
+}
+
+
+  
